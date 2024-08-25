15ee91231ded06a12d3fcbedcfafe4f61b6b6f09
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-25 17:32:56 -0400

    Create DellModules.ps1

---
+++ Individual Scripts/GUI-based/Dell/PowerShell Modules/DellModules.ps1
@@ -0,0 +1,42 @@
+Update-Module -Name DellBIOSProvider -Force 
+Import-Module -Name DellBIOSProvider -Force
+
+Update-Module -Name GetBIOS -Force 
+Import-Module -Name GetBIOS -Force
+
+Update-Module -Name Get-DellWarranty -Force 
+Import-Module -Name Get-DellWarranty -Force
+
+Update-Module -Name Dell.PowerStore -Force 
+Import-Module -Name Dell.PowerStore -Force
+
+Update-Module -Name DellOpenManage -Force 
+Import-Module -Name DellOpenManage -Force
+
+Update-Module -Name DellWarrantyServiceTag -Force 
+Import-Module -Name DellWarrantyServiceTag -Force
+
+Update-Module -Name Dell.Unity -Force 
+Import-Module -Name Dell.Unity -Force
+
+Update-Module -Name Dell.PowerMax -Force 
+Import-Module -Name Dell.PowerMax -Force
+
+Update-Module -Name DellBios -Force 
+Import-Module -Name DellBios -Force
+
+Update-Module -Name SetBIOS -Force 
+Import-Module -Name SetBIOS -Force
+
+Update-Module -Name DellWarranty -Force 
+Import-Module -Name DellWarranty -Force
+
+Update-Module -Name Get-DellSupportInfo -Force
+Import-Module -Name Get-DellSupportInfo -Force
+
+Update-Module -Name Get-DriversPackFromDell -Force 
+Import-Module -Name Get-DriversPackFromDell -Force
+
+Update-Module -Name Dell-BIOSDriver -Force 
+Import-Module -Name Dell-BIOSDriver -Force
+
