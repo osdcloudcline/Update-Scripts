da4aed8a398c8c52a47eaefb2956765f9af8734b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-25 17:37:25 -0400

    Create JENNPC-DellSoftware.ps1

---
+++ Individual Scripts/GUI-based/Dell/Micro Form Factor/Optiplex/3060/JENNPC-DellSoftware.ps1
@@ -0,0 +1,9 @@
+winget upgrade --id Dell.CommandUpdate.Universal --accept-package-agreements  --accept-source-agreements --force
+
+winget upgrade --id Dell.CommandUpdate --accept-package-agreements  --accept-source-agreements --force
+
+winget upgrade --id Dell.DisplayManager --accept-package-agreements  --accept-source-agreements --force
+
+winget upgrade --id Dell.CommandConfigure --accept-package-agreements  --accept-source-agreements --force
+
+winget upgrade --id 9PPRLNT023WC --accept-package-agreements  --accept-source-agreements --force
