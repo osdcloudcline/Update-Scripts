6e704cb6448112cfa8c42768189543bb855e2b91
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-25 17:51:44 -0400

    Update JENNPC.ps1

--- GUI-based Scripts/System-Specific Scripts/Micro Form Factor/Dell/Optiplex/JENNPC.ps1
+++ GUI-based Scripts/System-Specific Scripts/Micro Form Factor/Dell/Optiplex/JENNPC.ps1
@@ -1,47 +1,47 @@
 $JENNPCprocesses = @(
   @{
     Name = "Processing: Web Browsers"
-    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Chrome.ps1"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-Browsers.ps1"
     },
   @{
     Name = "Processing: Cloud Backups"
-    Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%205.1/Install.ps1"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-CloudBackup.ps1"
     },
   @{
    Name = "Processing: Media Software"
-   Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1"
+   Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-Media.ps1"
    },
   @{
   Name = "Processing: Messaging Software"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/AddSafeMode.ps1"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-Messaging.ps1"
   },
   @{
   Name = "Processing: Productivity Software"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/CheckForUpdates.ps1"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-Productivity.ps1"
   },
   @{
   Name = "Processing: VDI/Remote Desktop Software"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/CopyToMoveTo.ps1"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-VDIRemote.ps1"
   },
   @{
   Name = "Processing: System Utilities"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/DisableBitLockerDeviceEncryption.ps1"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-Utilities.ps1"
   },
   @{
   Name = "Processing: OS SYSTEM Software"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/DisableWindowsCopilot.ps1"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-OSSYSTEM.ps1"
   },
   @{
-  Name = "Processing: PowerShell 5.1 Modules"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/DisableWindowsRecall.ps1"
+  Name = "Processing: Dell Optiplex 3060 Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/Micro%20Form%20Factor/Optiplex/3060/JENNPC-DellSoftware.ps1"
   },
   @{
   Name = "Processing: Dell PowerShell Modules"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/OSDesktopIcons.ps1"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/PowerShell%20Modules/DellModules.ps1"
   },
   @{
   Name = "Processing: Updating Windows"
-  Script = ""
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/OS/UpdateOS.ps1"
   }
   )
 
