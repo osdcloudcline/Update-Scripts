2e6872f7c073bff41c1590b5eeef01fcbd8aa957
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-25 17:25:40 -0400

    Create JENNPC-OSSYSTEM.ps1

---
+++ Individual Scripts/GUI-based/Dell/Micro Form Factor/Optiplex/3060/JENNPC-OSSYSTEM.ps1
@@ -0,0 +1,51 @@
+winget upgrade --id Microsoft.AppInstaller
+
+winget upgrade --id Microsoft.Edge
+
+winget upgrade --id  Microsoft.DotNet.Runtime.3_1
+
+winget upgrade --id Microsoft.DotNet.Runtime.5
+
+winget upgrade --id Microsoft.DotNet.Runtime.6
+
+winget upgrade --id Microsoft.DotNet.Runtime.7
+
+winget upgrade --id Microsoft.DotNet.Runtime.8
+
+winget upgrade --id Oracle.JavaRuntimeEnvironment
+
+winget upgrade --id Microsoft.XNARedist
+
+winget upgrade --id Microsoft.VCRedist.2005.x64
+
+winget upgrade --id Microsoft.VCRedist.2005.x86
+
+winget upgrade --id Microsoft.VCRedist.2008.x64
+
+winget upgrade --id Microsoft.VCRedist.2008.x86
+
+winget upgrade --id Microsoft.VCRedist.2010.x64
+
+winget upgrade --id Microsoft.VCRedist.2010.x86
+
+winget upgrade --id Microsoft.VCRedist.2012.x64
+
+winget upgrade --id Microsoft.VCRedist.2012.x86
+
+winget upgrade --id Microsoft.VCRedist.2013.x64
+
+winget upgrade --id Microsoft.VCRedist.2013.x86
+
+choco upgrade vcredist2017 -y
+
+winget upgrade --id Microsoft.VCRedist.2015+.x64
+
+winget upgrade --id Microsoft.VCRedist.2015+.x86
+
+choco upgrade dotnetfx -y
+
+choco upgrade openal -y
+
+winget upgrade --id Microsoft.WindowsTerminal
+
+winget upgrade --id Microsoft.PowerShell
