7adac983e7e508865d4b3a1a6bc46063f18227d7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-26 11:24:55 -0400

    Update DellModules.ps1

--- Individual Scripts/GUI-based/Dell/PowerShell Modules/DellModules.ps1
+++ Individual Scripts/GUI-based/Dell/PowerShell Modules/DellModules.ps1
@@ -1,3 +1,46 @@
+Install-Module -Name DellBIOSProvider -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name DellBIOSProvider -Force
+
+Install-Module -Name GetBIOS -Force -AllowClobber -SkipPublisherCheck 
+Import-Module -Name GetBIOS -Force
+
+Install-Module -Name Get-DellWarranty -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name Get-DellWarranty -Force
+
+Install-Module -Name Dell.PowerStore -Force -AllowClobber -SkipPublisherCheck 
+Import-Module -Name Dell.PowerStore -Force
+
+Install-Module -Name DellOpenManage -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name DellOpenManage -Force
+
+Install-Module -Name DellWarrantyServiceTag -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name DellWarrantyServiceTag -Force
+
+Install-Module -Name Dell.Unity -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name Dell.Unity -Force
+
+Install-Module -Name Dell.PowerMax -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name Dell.PowerMax -Force
+
+Install-Module -Name DellBios -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name DellBios -Force
+
+Install-Module -Name SetBIOS -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name SetBIOS -Force
+
+Install-Module -Name DellWarranty -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name DellWarranty -Force
+
+Install-Module -Name Get-DellSupportInfo -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name Get-DellSupportInfo -Force
+
+Install-Module -Name Get-DriversPackFromDell -Force -AllowClobber -SkipPublisherCheck 
+Import-Module -Name Get-DriversPackFromDell -Force
+
+Install-Module -Name Dell-BIOSDriver -Force -AllowClobber -SkipPublisherCheck 
+Import-Module -Name Dell-BIOSDriver -Force
+
+
 Update-Module -Name DellBIOSProvider -Force 
 Import-Module -Name DellBIOSProvider -Force
 
