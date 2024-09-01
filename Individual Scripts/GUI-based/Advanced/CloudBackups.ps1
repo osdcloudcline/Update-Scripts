6b18644e8c0df4342f9b5b22846a3d0598219989
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 11:59:32 -0400

    Update CloudBackups.ps1

--- Individual Scripts/GUI-based/Advanced/CloudBackups.ps1
+++ Individual Scripts/GUI-based/Advanced/CloudBackups.ps1
@@ -1,1 +1,15 @@
+$CloudBackupssUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-CloudBackups.log"
 
+Start-Transcript -Path $CloudBackupssUpgradeLog
+
+winget upgrade --id Dropbox.Dropbox --source winget --force
+
+winget upgrade --id Google.GoogleDrive --source winget --force
+
+winget upgrade --id Nextcloud.NextcloudDesktop --source winget --force
+
+winget upgrade --id Microsoft.OneDrive --source winget --force
+
+winget upgrade --id Apple.iCloud --source winget --force
+
+Stop-Transcript
