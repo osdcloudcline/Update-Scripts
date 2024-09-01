5f61948650dd9fdbea9cea804b48199fd1a3dcc0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 11:54:56 -0400

    Update Browsers.ps1

--- Individual Scripts/GUI-based/Advanced/Browsers.ps1
+++ Individual Scripts/GUI-based/Advanced/Browsers.ps1
@@ -1,1 +1,9 @@
+$BrowsersUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-Browsers.log"
 
+Start-Transcript -Path $BrowsersUpgradeLog
+
+winget upgrade --id Google.Chrome --source winget --force
+
+winget upgrade --id Mozilla.Firefox --source winget --force
+
+Stop-Transcript
