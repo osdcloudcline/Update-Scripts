8be0a21d006f5b1c385b769107a585e20a9f52bd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 18:04:29 -0400

    Update RemoteConnections.ps1

--- Individual Scripts/GUI-based/Advanced/RemoteConnections.ps1
+++ Individual Scripts/GUI-based/Advanced/RemoteConnections.ps1
@@ -1,1 +1,13 @@
+$RemoteConnectionsUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-RemoteConnections.log"
 
+Start-Transcript -Path $RemoteConnectionsUpgradeLog
+
+winget upgrade --id AnyDeskSoftwareGmbH.AnyDesk --source winget --force
+
+winget upgrade --id Microsoft.RemoteDesktopClient --source winget --force 
+
+choco upgrade vmware-horizon-client -y
+
+winget upgrade --id 9N1F85V9T8BN --source msstore --force
+
+Stop-Transcript
