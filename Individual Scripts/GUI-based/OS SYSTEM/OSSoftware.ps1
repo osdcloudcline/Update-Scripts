2ae2122e2d2a8ecabb5525ecbb80b51ab5564af0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 15:52:20 -0400

    Update OSSoftware.ps1

--- Individual Scripts/GUI-based/OS SYSTEM/OSSoftware.ps1
+++ Individual Scripts/GUI-based/OS SYSTEM/OSSoftware.ps1
@@ -4,10 +4,54 @@ Start-Transcript -Path $OSSoftwareUpgradeLog
 
 winget upgrade --id Microsoft.AppInstaller --source winget -force
 
-winget upgrade --id Microsoft.Edge
+winget upgrade --id Microsoft.Edge --source winget -force
 
-winget upgrade --id  Microsoft.DotNet.Runtime.3_1
+winget upgrade --id  Microsoft.DotNet.Runtime.3_1 --source winget -force
 
-winget upgrade --id Microsoft.DotNet.Runtime.5
+winget upgrade --id Microsoft.DotNet.Runtime.5 --source winget -force
 
-winget upgrade --id Microsoft.DotNet.Runtime.6
+winget upgrade --id Microsoft.DotNet.Runtime.6 --source winget -force
+
+winget upgrade --id Microsoft.DotNet.Runtime.7 --source winget -force
+
+winget upgrade --id Microsoft.DotNet.Runtime.8 --source winget -force
+
+winget upgrade --id Oracle.JavaRuntimeEnvironment --source winget -force
+
+winget upgrade --id Microsoft.XNARedist --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2005.x64 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2005.x86 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2008.x64 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2008.x86 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2010.x64 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2010.x86 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2012.x64 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2012.x86 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2013.x64 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2013.x86 --source winget -force
+
+choco upgrade vcredist2017 -y
+
+winget upgrade --id Microsoft.VCRedist.2015+.x64 --source winget -force
+
+winget upgrade --id Microsoft.VCRedist.2015+.x86 --source winget -force
+
+choco upgrade dotnetfx -y
+
+choco upgrade openal -y
+
+winget upgrade --id Microsoft.WindowsTerminal --source winget -force
+
+winget upgrade --id Microsoft.PowerShell --source winget -force
+
+Stop-Transcript
