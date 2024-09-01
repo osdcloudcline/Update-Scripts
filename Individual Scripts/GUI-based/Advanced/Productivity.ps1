05a930b4e52a844035ef5539375e379e1738d7e1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 17:58:58 -0400

    Update Productivity.ps1

--- Individual Scripts/GUI-based/Advanced/Productivity.ps1
+++ Individual Scripts/GUI-based/Advanced/Productivity.ps1
@@ -1,1 +1,7 @@
+$ProductivityUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-Productivity.log"
 
+Start-Transcript -Path $ProductivityUpgradeLog
+
+winget upgrade --id Adobe.Acrobat.Reader.64-bit --source winget --force
+
+Stop-Transcript
