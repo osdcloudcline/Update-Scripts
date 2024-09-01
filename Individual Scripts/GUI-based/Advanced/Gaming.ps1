9a7a53fc2ecfaf18e848e8fe5a7cb931224ca3e4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 14:40:55 -0400

    Update Gaming.ps1

--- Individual Scripts/GUI-based/Advanced/Gaming.ps1
+++ Individual Scripts/GUI-based/Advanced/Gaming.ps1
@@ -1,1 +1,7 @@
+$GamingUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-Gaming.log"
 
+Start-Transcript -Path $GamingUpgradeLog
+
+winget upgrade --id Valve.Steam --source winget --force
+
+Stop-Transcript
