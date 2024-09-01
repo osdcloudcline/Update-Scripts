dc7cedc72ca17f28a15173eaaaf2a9e01b38391f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 15:40:41 -0400

    Update OSSoftware.ps1

--- Individual Scripts/GUI-based/Advanced/OSSoftware.ps1
+++ Individual Scripts/GUI-based/Advanced/OSSoftware.ps1
@@ -1,1 +1,15 @@
+$OSSoftwareUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-OSSoftware.log"
+
+Start-Transcript -Path $OSSoftwareUpgradeLog
+
+winget upgrade --id Microsoft.AppInstaller --source winget -force
+
+winget upgrade --id Microsoft.Edge
+
+winget upgrade --id  Microsoft.DotNet.Runtime.3_1
+
+winget upgrade --id Microsoft.DotNet.Runtime.5
+
+winget upgrade --id Microsoft.DotNet.Runtime.6
+
 
