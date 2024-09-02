4d78ecee72ba74f771ac41282f9406c87fe62916
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 06:26:27 -0400

    Update Programming.ps1

--- Individual Scripts/GUI-based/Management PC/Programming.ps1
+++ Individual Scripts/GUI-based/Management PC/Programming.ps1
@@ -1,1 +1,9 @@
+$ProgrammingUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-Programming.log"
 
+Start-Transcript -Path $ProgrammingUpgradeLog
+
+winget upgrade --id CursorAI,Inc.Cursor --source winget --force
+
+winget upgrade --id Microsoft.VisualStudioCode --source winget --force
+
+Stop-Transcript
