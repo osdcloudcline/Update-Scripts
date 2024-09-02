abc9eff03aa799696b5c7f0983b877cecd90046e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 00:45:08 -0400

    Create Utilities.ps1

---
+++ Individual Scripts/GUI-based/IT/Utilities.ps1
@@ -0,0 +1,11 @@
+$UtilitiesUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-Utilities.log"
+
+Start-Transcript -Path $UtilitiesUpgradeLog
+
+winget upgrade --id 7zip.7zip --source winget --force
+
+winget upgrade --id Piriform.CCleaner --source winget --force
+
+winget upgrade --id Wagnardsoft.DisplayDriverUninstaller --source winget --force
+
+Stop-Transcript
