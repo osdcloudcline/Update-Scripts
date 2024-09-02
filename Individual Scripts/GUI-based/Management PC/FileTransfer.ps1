8c05488ced37ce0e00a38e420cc409d7591a24da
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 06:25:37 -0400

    Update FileTransfer.ps1

--- Individual Scripts/GUI-based/Management PC/FileTransfer.ps1
+++ Individual Scripts/GUI-based/Management PC/FileTransfer.ps1
@@ -1,1 +1,11 @@
+$FileTransferUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-FileTransfer.log"
 
+Start-Transcript -Path $FileTransferUpgradeLog
+
+choco upgrade filezilla  -y
+
+winget upgrade --id PuTTY.PuTTY --source winget --force
+
+winget upgrade --id WinSCP.WinSCP --source winget --force
+
+Stop-Transcript
