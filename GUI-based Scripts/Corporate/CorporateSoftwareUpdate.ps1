f18e322abec2d7b19fe850166132c1bcca43a96d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 00:21:29 -0400

    Update CorporateSoftwareUpdate.ps1

--- GUI-based Scripts/Corporate/CorporateSoftwareUpdate.ps1
+++ GUI-based Scripts/Corporate/CorporateSoftwareUpdate.ps1
@@ -1,1 +1,48 @@
-
+$CORPprocesses = @(
+  @{
+    Name = "Processing: Web Browsers"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Corporate/Browsers.ps1"
+    },
+  @{
+    Name = "Processing: Cloud Backups"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Corporate/CloudBackups.ps1"
+    },
+  @{
+   Name = "Processing: Media Software"
+   Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Corporate/Media.ps1"
+   },
+  @{
+  Name = "Processing: Productivity Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Corporate/Productivity.ps1"
+  },
+  @{
+  Name = "Processing: VDI/Remote Desktop Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Corporate/RemoteConnections.ps1"
+  },
+  @{
+  Name = "Processing: System Utilities"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Corporate/Utilities.ps1"
+  },
+  @{
+  Name = "Processing: OS SYSTEM Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/OS%20SYSTEM/OSSoftware.ps1"
+  },
+  @{
+  Name = "Processing: PowerShell 5.1 Modules"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/PowerShell/Modules%20-%20version%205.1/PowerShell51Modules.ps1"
+  },
+  @{
+  Name = "Processing: PowerShell 7.x.x Modules"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/PowerShell/Modules%20-%20version%207.x.x/PowerShell7xModules.ps1"
+  },
+    @{
+  Name = "Processing: PowerShell Providers"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/PowerShell/Providers/PowerShellProviders.ps1"
+  },
+  @{
+  Name = "Processing: Updating Windows"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/OS/UpdateOS.ps1"
+  }
+  )
+  
+  Start-SplashScreen -Processes $CORPprocesses -MessageHeader "Corporate System Configuration - Updating: $env:computername"
