f6ff4979cea75bcf3c0016d990923640e4baf7f6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 07:07:23 -0400

    Update StandardSystemUpdate.ps1

--- GUI-based Scripts/Standard/StandardSystemUpdate.ps1
+++ GUI-based Scripts/Standard/StandardSystemUpdate.ps1
@@ -1,1 +1,64 @@
-
+$STDprocesses = @(
+  @{
+    Name = "Processing: Web Browsers"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Browsers.ps1"
+    },
+  @{
+    Name = "Processing: Cloud Backups"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/CloudBackups.ps1"
+    },
+  @{
+    Name = "Processing: File Transfer Software"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/FileTransfer.ps1"
+    },
+  @{
+    Name = "Processing: Gaming Software"
+    Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Gaming.ps1"
+    },
+  @{
+   Name = "Processing: Media Software"
+   Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Media.ps1"
+   },
+  @{
+  Name = "Processing: Productivity Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Productivity.ps1"
+  },
+  @{
+  Name = "Processing: Programming Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Programming.ps1"
+  },
+  @{
+  Name = "Processing: VDI/Remote Desktop Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/RemoteConnections.ps1"
+  },
+  @{
+  Name = "Processing: Virtualization Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Virtualization.ps1"
+  },
+  @{
+  Name = "Processing: System Utilities"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Standard/Utilities.ps1"
+  },
+  @{
+  Name = "Processing: OS SYSTEM Software"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/OS%20SYSTEM/OSSoftware.ps1"
+  },
+  @{
+  Name = "Processing: PowerShell 5.1 Modules"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/PowerShell/Modules%20-%20version%205.1/PowerShell51Modules.ps1"
+  },
+  @{
+  Name = "Processing: PowerShell 7.x.x Modules"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/PowerShell/Modules%20-%20version%207.x.x/PowerShell7xModules.ps1"
+  },
+    @{
+  Name = "Processing: PowerShell Providers"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/PowerShell/Providers/PowerShellProviders.ps1"
+  },
+  @{
+  Name = "Processing: Updating Windows"
+  Script = "https://github.com/osdcloudcline/Update-Scripts/raw/main/Individual%20Scripts/GUI-based/Dell/OS/UpdateOS.ps1"
+  }
+  )
+  
+  Start-SplashScreen -Processes $ADVprocesses -MessageHeader "Standard System Configuration - Updating: $env:computername"
