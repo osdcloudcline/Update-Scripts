7482f156fcc3f2afdff2f85f9395d40c1cc6ac43
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 09:48:14 -0400

    Update FileTransfer.ps1

--- Individual Scripts/GUI-based/Standard/FileTransfer.ps1
+++ Individual Scripts/GUI-based/Standard/FileTransfer.ps1
@@ -2,6 +2,14 @@ $FileTransferUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-FileT
 
 Start-Transcript -Path $FileTransferUpgradeLog
 
+$FZSQLFile = "C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3"
+$FZSQLDetect = [System.IO.File]::Exists("C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3")
+If($FZSQLDetect -eq $true){
+Remove-Item -Path $FZSQLFile -Force
 choco upgrade filezilla  -y
+}
+Elseif ($FZSQLDetect -eq $false){
+choco upgrade filezilla  -y
+}
 
 Stop-Transcript
