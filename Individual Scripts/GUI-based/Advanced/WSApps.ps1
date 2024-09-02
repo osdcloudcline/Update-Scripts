99dade1e9223051d98e1c9e30166c1a20d1559b4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 21:16:37 -0400

    Update WSApps.ps1

--- Individual Scripts/GUI-based/Advanced/WSApps.ps1
+++ Individual Scripts/GUI-based/Advanced/WSApps.ps1
@@ -1,1 +1,31 @@
+$WSAppsUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-WSApps.log"
 
+Start-Transcript -Path $WSAppsUpgradeLog
+
+winget upgrade --id 9WZDNCRD29V9 --source msstore
+
+winget upgrade --id 9WZDNCRFJ3PZ --source msstore
+
+winget upgrade --id 9NCVDN91XZQP --source msstore
+
+winget upgrade --id 9WZDNCRFJ3TJ --source msstore
+
+winget upgrade --id 9NWD3S85MTFB --source msstore
+
+winget upgrade --id 9P6RC76MSMMJ --source msstore
+
+winget upgrade --id 9NXQXXLFST89 --source msstore
+
+winget upgrade --id 9WZDNCRFJ3L1 --source msstore
+
+winget upgrade --id 9NM4T8B9JQZ1 --source msstore
+
+winget upgrade --id 9WZDNCRFJ0WK --source msstore
+
+winget upgrade --id 9PL67R4P9PG5 --source msstore
+
+winget upgrade --id 9NW6L8PLQMM4 --source msstore
+
+winget upgrade --id 9PM860492SZD --source msstore
+
+Stop-Transcript
