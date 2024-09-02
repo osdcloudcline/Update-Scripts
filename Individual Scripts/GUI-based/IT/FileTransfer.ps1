731b6fd4fdaba448aae9266e179a470beca0304f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 09:47:16 -0400

    Update FileTransfer.ps1

--- Individual Scripts/GUI-based/IT/FileTransfer.ps1
+++ Individual Scripts/GUI-based/IT/FileTransfer.ps1
@@ -2,7 +2,15 @@ $FileTransferUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-FileT
 
 Start-Transcript -Path $FileTransferUpgradeLog
 
+$FZSQLFile = "C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3"
+$FZSQLDetect = [System.IO.File]::Exists("C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3")
+If($FZSQLDetect -eq $true){
+Remove-Item -Path $FZSQLFile -Force
 choco upgrade filezilla  -y
+}
+Elseif ($FZSQLDetect -eq $false){
+choco upgrade filezilla  -y
+}
 
 winget upgrade --id PuTTY.PuTTY --source winget --force
 
