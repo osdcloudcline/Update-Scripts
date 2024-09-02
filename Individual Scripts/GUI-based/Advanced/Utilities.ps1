af307b5f394f8ecde0ec24c21bc35e2cd851ee77
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-01 21:10:49 -0400

    Update Utilities.ps1

--- Individual Scripts/GUI-based/Advanced/Utilities.ps1
+++ Individual Scripts/GUI-based/Advanced/Utilities.ps1
@@ -1,1 +1,37 @@
+$UtilitiesUpgradeLog = "C:\Logs\Upgrades\Software\GUI\$env:computername-Utilities.log"
 
+Start-Transcript -Path $RemoteConnectionsUpgradeLog
+
+winget upgrade --id 7zip.7zip --source winget --force
+
+winget upgrade --id Piriform.CCleaner --source winget --force
+
+winget upgrade --id Wagnardsoft.DisplayDriverUninstaller --source winget --force
+
+winget upgrade --id IObit.DriverBooster --source winget --force
+
+winget upgrade --id Rem0o.FanControl --source winget --force
+
+winget upgrade --id Logitech.Options --source winget --force
+
+winget upgrade --id Logitech.UnifyingSoftware --source winget --force
+
+winget upgrade --id Microsoft.MouseandKeyboardCenter --source winget --force
+
+winget upgrade --id Rufus.Rufus --source winget --force
+
+winget upgrade --id Samsung.SamsungMagician --source winget --force
+
+winget upgrade --id Microsoft.WindowsAdminCenter --source winget --force
+
+winget upgrade --id XPDLS1XBTXVPP4 --source msstore --source winget --force
+
+Stop-Transcript
+
+$PCName = $env:computername 
+
+If($PCName -eq "BTDESKTOP")}
+
+
+
+}
