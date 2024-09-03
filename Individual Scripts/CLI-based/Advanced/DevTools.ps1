f8ae38959a30a59caebd58c176c5041c007a0dac
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 22:46:41 -0400

    Update DevTools.ps1

--- Individual Scripts/CLI-based/Advanced/DevTools.ps1
+++ Individual Scripts/CLI-based/Advanced/DevTools.ps1
@@ -2,7 +2,8 @@ $app1 = "Docker Desktop"
 $app2 = "GitHub Desktop"
 
 
-
+Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id Docker.DockerDesktop --source winget --force
 
+Write-Host "Checking for and processing update: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id GitHub.GitHubDesktop --source winget --force
