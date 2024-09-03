2c5deccc81bda63174c3f690ece49b59477fd215
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-02 22:49:38 -0400

    Update Gaming.ps1

--- Individual Scripts/CLI-based/Advanced/Gaming.ps1
+++ Individual Scripts/CLI-based/Advanced/Gaming.ps1
@@ -1,4 +1,6 @@
 $app1 = "Steam Online Gaming Client"
 
+
+Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id Valve.Steam --source winget --force
 
