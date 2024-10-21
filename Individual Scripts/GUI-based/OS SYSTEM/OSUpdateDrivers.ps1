951ca918fd2c40187987b5b3197c9c9d8e0002ab
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-21 11:19:14 -0400

    Update OSUpdateDrivers.ps1

--- Individual Scripts/GUI-based/OS SYSTEM/OSUpdateDrivers.ps1
+++ Individual Scripts/GUI-based/OS SYSTEM/OSUpdateDrivers.ps1
@@ -1,1 +1,4 @@
-Get-WindowsUpdate -UpdateType Driver -AcceptAll -ForceInstall 
+Install-Module -Name PSWindowsUpdate -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name PSWindowsUpdate -Force
+
+Get-WindowsUpdate -UpdateType Driver -AcceptAll -Install 
