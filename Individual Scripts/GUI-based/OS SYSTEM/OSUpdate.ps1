4e7bf88fd5243ba480083f599273876c0648034e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-21 11:17:59 -0400

    Update OSUpdate.ps1

--- Individual Scripts/GUI-based/OS SYSTEM/OSUpdate.ps1
+++ Individual Scripts/GUI-based/OS SYSTEM/OSUpdate.ps1
@@ -1,1 +1,4 @@
+Install-Module -Name PSWindowsUpdate -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name PSWindowsUpdate -Force
+
 Get-WindowsUpdate -UpdateType Software -AcceptAll -ForceInstall
