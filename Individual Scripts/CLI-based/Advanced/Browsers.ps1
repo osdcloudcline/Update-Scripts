84cac9086df528edc62e326485d929c6cdbac6ac
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-21 22:27:53 -0500

    Update Browsers.ps1

--- Individual Scripts/CLI-based/Advanced/Browsers.ps1
+++ Individual Scripts/CLI-based/Advanced/Browsers.ps1
@@ -1,9 +1,24 @@
 $app1 = "Google Chrome for Enterprise"
 
+$TestChrome = (Test-Path -Path "C:\Program Files\Google\Chrome\Application\chrome.exe" -IsValid)
+
+If($TestChrome -eq $true){
 Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id Google.Chrome --source winget --force
+}
+ElseIf($TestChrome -eq $false){
+Write-Verbose "$app1 is not installed. Skipping update..." -Verbose
+}
+
 
 $app2 = "Mozilla Firefox"
 
+$TestFirefox = (Test-Path -Path "C:\Program Files\Mozilla Firefox\firefox.exe" -IsValid)
+
+If($TestFirefox -eq $true){
 Write-Host "Checking for and processing update: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id Mozilla.Firefox --source winget --force
+}
+ElseIf($TestFirefox -eq $false){
+Write-Verbose "$app2 is not installed. Skipping update..." -Verbose
+}
